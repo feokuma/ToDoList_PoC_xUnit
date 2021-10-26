46122775d62a7dcd46c041adce71d724212a0123
Fernando Okuma <feokuma@gmail.com>
2021-10-26 01:29:35 -0300

    :construction: Implementa repositório genérico

--- src/Startup.cs
+++ src/Startup.cs
@@ -7,63 +7,65 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using ToDoApp.Data;
+using ToDoApp.Models;
+using ToDoApp.Repositories;
 
 namespace ToDoApp
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddApiVersioning(p =>
-            {
-                p.DefaultApiVersion = new ApiVersion(1, 0);
-                p.ReportApiVersions = true;
-                p.AssumeDefaultVersionWhenUnspecified = true;
-            });
-            services.AddVersionedApiExplorer(p =>
-            {
-                p.GroupNameFormat = "'v'VVV";
-                p.SubstituteApiVersionInUrl = true;
-            });
-            services.AddControllers();
-            services.AddDbContext<AppDbContext>();
-            services.AddSwaggerGen();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddApiVersioning(p =>
+			{
+				p.DefaultApiVersion = new ApiVersion(1, 0);
+				p.ReportApiVersions = true;
+				p.AssumeDefaultVersionWhenUnspecified = true;
+			});
+			services.AddVersionedApiExplorer(p =>
+			{
+				p.GroupNameFormat = "'v'VVV";
+				p.SubstituteApiVersionInUrl = true;
+			});
+			services.AddControllers();
+			services.AddDbContext<AppDbContext>();
+			services.AddSwaggerGen();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(options =>
-                {
-                    foreach (var description in provider.ApiVersionDescriptions)
-                    {
-                        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
-                    }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseSwagger();
+				app.UseSwaggerUI(options =>
+				{
+					foreach (var description in provider.ApiVersionDescriptions)
+					{
+						options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+					}
 
-                    options.DocExpansion(DocExpansion.List);
-                });
-            }
+					options.DocExpansion(DocExpansion.List);
+				});
+			}
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
 }
