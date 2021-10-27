95844b72ddaf1f058e50cf5972288dca7be115b6
Fernando Okuma <feokuma@gmail.com>
2021-10-27 03:08:12 -0300

    :building_construction: Adiciona configuração para impedir paralelismo testes

--- test/ToDoApp.Integration/Setup/TestBase.cs
+++ test/ToDoApp.Integration/Setup/TestBase.cs
@@ -6,40 +6,42 @@ using Respawn;
 using ToDoApp.Data;
 using Xunit;
 
+// Evita que duas classes de teste executem o Construtor e o InitializeAsync ao mesmo tempo
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 namespace ToDoApp.Integration.Setup
 {
-    public abstract class TestBase : IClassFixture<ApiWebApplicationFactory>, IAsyncLifetime
-    {
-        public HttpClient Client { get; set; }
-        public AppDbContext Context { get; }
+	public abstract class TestBase : IClassFixture<ApiWebApplicationFactory>, IAsyncLifetime
+	{
+		public HttpClient Client { get; set; }
+		public AppDbContext Context { get; }
 
-        public readonly Checkpoint Checkpoint = new()
-        {
-            TablesToIgnore = new[]{
-                "_EFMigrationsHistory"
-            },
-            DbAdapter = DbAdapter.Postgres
-        };
+		public readonly Checkpoint Checkpoint = new()
+		{
+			TablesToIgnore = new[]{
+				"_EFMigrationsHistory"
+			},
+			DbAdapter = DbAdapter.Postgres
+		};
 
-        // Este código será executado antes de cada teste e antes do InitializeAsync
-        public TestBase(ApiWebApplicationFactory factory)
-        {
-            Client = factory.CreateClient();
-            Client.BaseAddress = new Uri("http://localhost:5000/api/");
-            Context = factory.Context;
-        }
+		// Este código será executado antes de cada teste e antes do InitializeAsync
+		public TestBase(ApiWebApplicationFactory factory)
+		{
+			Client = factory.CreateClient();
+			Client.BaseAddress = new Uri("http://localhost:5000/api/");
+			Context = factory.Context;
+		}
 
-        // Este código será executado antes de cada teste e depois do construtor TestBase e do construtor da classe filha
-        public async Task InitializeAsync()
-        {
-            await Context.Database.EnsureCreatedAsync();
-            Context.ChangeTracker.Clear();
-            using var npgsqlConnection = new NpgsqlConnection(Context.ConnectionString);
-            await npgsqlConnection.OpenAsync();
-            await Checkpoint.Reset(npgsqlConnection);
-        }
+		// Este código será executado antes de cada teste e depois do construtor TestBase e do construtor da classe filha
+		public async Task InitializeAsync()
+		{
+			await Context.Database.EnsureCreatedAsync();
+			Context.ChangeTracker.Clear();
+			using var npgsqlConnection = new NpgsqlConnection(Context.ConnectionString);
+			await npgsqlConnection.OpenAsync();
+			await Checkpoint.Reset(npgsqlConnection);
+		}
 
-        // O DisposeAsync será executado depois de cada teste
-        public async Task DisposeAsync() => await Task.CompletedTask;
-    }
+		// O DisposeAsync será executado depois de cada teste
+		public async Task DisposeAsync() => await Task.CompletedTask;
+	}
 }
